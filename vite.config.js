9e58d52a680d8dfbae65926f028aa6fd72fb00b6
wilsonteoshiyang@gmail.com <40634566+Wilson1113@users.noreply.github.com>
2024-09-06 00:56:42 +0800

    Update wallet connection
    - Able to connect with wallet

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Birdrop/",
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 })
